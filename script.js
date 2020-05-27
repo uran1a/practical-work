a36d3af47a1a6da7d68c86405f53334deec1cc9f
Nikita <9797171z@mail.ru>
2020-05-27 15:16:45 +0600

    first practical work

---
+++ script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
